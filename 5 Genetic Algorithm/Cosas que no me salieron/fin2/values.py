58f944611bd12574d47db44ebe1b2ce202d52be6
heritaco <154515410+heritaco@users.noreply.github.com>
2024-04-30 14:18:51 -0600

    :)

--- 5 Genetic Algorithm/Cosas que no me salieron/fin2/values.py
+++
@@ -1,8 +0,0 @@
-
-start_point = 'El Rosario'
-end_point = 'San Lázaro'
-size_pop = 10
-num_generations = 200
-elite_size = 2
-mutation_rate = 0.5
-# Genome
--- 5 Genetic Algorithm/Cosas que no me salieron/parameters.py
+++
@@ -1,6 +0,0 @@
-start_point = 'El Rosario'
-end_point = 'San Lázaro'
-size_pop = 12           # at least 2
-num_generations = 100   # does not affect that much the time of execution, i reommend at least 100 generations to get a good solution
-elite_size = 4          # at least 2
-mutation_rate = 1       # between 0 and 1, i recommend 1 because we always have a copy of the best genome
